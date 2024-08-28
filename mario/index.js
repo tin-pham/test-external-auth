54f2c8ceb36c05c5b49b159ea8ec35f939f3fc1f
tin-pham <heoconlinhat123@gmail.com>
2024-08-28 17:08:01 +0700

    auth not working

---
+++ mario/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 8082;
+
+app.get("/mario", (req, res) => {
+  res.send("Mario");
+});
+
+app.listen(port, () => {
+  console.log(`Mario app listening at http://localhost:${port}`);
+});
